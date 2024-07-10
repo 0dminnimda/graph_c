ab8b569879a8a8b48d2d3a337ac220d9a453efa8
0dminnimda <0dminnimda@gmail.com>
2024-07-10 19:06:25 +0300

    array.h: add recursive deinit, copy and find

--- src/basic/array.h
+++ src/basic/array.h
@@ -5,6 +5,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 #if !defined(TOKENPASTE_) && !defined(TOKENPASTE)
 #define TOKENPASTE_(x, y) x ## y
@@ -18,6 +19,9 @@
 }
 
 #define ARRAY_APPEND_INIT_CAP 128
+
+#define array_it_ TOKENPASTE(it__, __LINE__)
+#define array_ind_ TOKENPASTE(TOKENPASTE(it__, __LINE__), _index)
 
 #define array_item_size(self) sizeof(*(self)->data)
 
@@ -35,6 +39,28 @@
 
 #define array_deinit(self) free((self)->data)
 
+#define array_deinit_with_item(self, T) do { \
+    T *array_it_; \
+    array_for((self), array_it_) { \
+        T##_deinit(array_it_); \
+    } \
+    array_deinit((self)); \
+} while(0)
+
+#define array_copy(self, other) ( \
+    (other)->data = malloc((self)->length * array_item_size((self))), \
+    (other)->length = (self)->length, \
+    (other)->capacity = (self)->length, \
+    memcpy((other)->data, (self)->data, (self)->length * array_item_size((self))) \
+)
+
+#define array_copy_with_item(self, T, other) do { \
+    array_copy((self), (other)); \
+    T *array_it_; \
+    array_for((self), array_it_) { \
+        T##_copy(&(other)->data[array_ind_], array_it_); \
+    } \
+} while(0)
 
 #define array_expand(self, cap) ( \
     (self)->data = realloc((self)->data, (cap) * array_item_size((self))), \
@@ -68,6 +94,18 @@
     (index != (self)->length)? \
         ((self)->data[index] = (self)->data[(self)->length], 0): 0 \
 )
+
+/* result is a pointer to the item in the array */
+#define array_find(self, T, item, op, result) do { \
+    T *array_it_; \
+    result = NULL; \
+    array_for((self), array_it_) { \
+        if (T##_compare((item), array_it_) op 0) { \
+            result = array_it_; \
+            break; \
+        } \
+    } \
+} while(0)
 
 #define array_for(self, name) \
     if ((name = (self)->data), 1) \
