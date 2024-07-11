d653d9dc023a5a836e700c7f803e982ad76e4016
0dminnimda <0dminnimda@gmail.com>
2024-07-11 16:06:43 +0300

    str_builder: reserve only one more character on get()

--- src/basic/str_builder.c
+++ src/basic/str_builder.c
@@ -1,12 +1,13 @@
 #include "str_builder.h"
+#include "array.h"
 
 void str_builder_add(StrBuilder *sb, const str *string) {
     array_extend_from(sb, string);
 }
 
 str str_builder_get(StrBuilder *sb) {
-    *array_add(sb) = '\0';
-    --sb->length;
+    array_reserve(sb, sb->length + 1);
+    sb->data[sb->length] = '\0';
 
     str result;
     str_init(&result, sb->data, sb->length);
