4a0936b358cc634ee68dba7e7d7c16278fe9fa2b
0dminnimda <0dminnimda@gmail.com>
2024-07-11 15:01:08 +0300

    Move integer type limits into the types.h

--- src/basic/checked_arithmetic.h
+++ src/basic/checked_arithmetic.h
@@ -1,20 +1,5 @@
 #include <limits.h>
-
-#define I8_MAX 127
-#define I8_MIN -128
-#define U8_MAX 255
-
-#define I16_MAX 32767
-#define I16_MIN -32768
-#define U16_MAX 65535
-
-#define I32_MAX 2147483647
-#define I32_MIN −2147483648
-#define U32_MAX 4294967295
-
-#define I64_MAX 9223372036854775807
-#define I64_MIN −9223372036854775808
-#define U64_MAX 18446744073709551615
+#include "types.h"
 
 #if __has_include(<stdckdint.h>)
 #  include <stdckdint.h>
--- src/basic/types.h
+++ src/basic/types.h
@@ -39,4 +39,21 @@ typedef long double f80;
 #define PRI_f64 "%lf"
 #define PRI_f80 "%Lf"
 
+/* Their limits. Useful to have in one place. */
+#define I8_MAX 127
+#define I8_MIN -128
+#define U8_MAX 255
+
+#define I16_MAX 32767
+#define I16_MIN -32768
+#define U16_MAX 65535
+
+#define I32_MAX 2147483647
+#define I32_MIN −2147483648
+#define U32_MAX 4294967295
+
+#define I64_MAX 9223372036854775807
+#define I64_MIN −9223372036854775808
+#define U64_MAX 18446744073709551615
+
 #endif  // TYPES_H
