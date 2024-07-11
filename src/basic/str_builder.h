d140157d0a51fb921802033d3df1efeb8458009d
0dminnimda <0dminnimda@gmail.com>
2024-07-11 15:52:57 +0300

    str_builder: add comment

--- src/basic/str_builder.h
+++ src/basic/str_builder.h
@@ -10,6 +10,11 @@ typedef array(char) StrBuilder;
 
 /* For init and deinit use array.h */
 void str_builder_add(StrBuilder *sb, const str *string);
+
+/* Will make a slice of the data in the builder.
+ * Will add the null terminator afted the last (at index length),
+ * but it will not increase the length.
+ * It's there just in case anyone will use data as cstr. */
 str str_builder_get(StrBuilder *sb);
 
 #endif // STR_BUILDER_H
